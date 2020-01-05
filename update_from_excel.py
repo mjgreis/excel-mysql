24fb3799b069cc41c4a6c43e1d47ba589628fa00
mjgreis <mgreis@comcast.net>
2020-01-05 00:41:43 -0500

    Run update for PV potential
    This variant executes an update to add rs_pv_potl to the RESIDENCE table based on the information in the Roof Survey spreadsheet.

--- update_from_excel.py
+++ update_from_excel.py
@@ -1,5 +1,8 @@
 # insert_update_from_excel
 # 2019-12-29
+#
+# Branch version 2020-01-04 - Update rs_pv_potl field
+#
 # Update a MySQL table using information loaded from an Excel spreadsheet
 # Written to update Needham residence table with information about solar
 # 
@@ -8,7 +11,7 @@ import xlrd
 import mysql.connector
 #
 # Open Excel workbook; read sheet into "sheet"
-book = xlrd.open_workbook(r"C:\Documents\Needham\Green Needham\Projects\Solarize Plus\Installed_PV_for_Needham_DB.xlsx")
+book = xlrd.open_workbook(r"C:\Documents\Needham\Green Needham\Projects\Solarize Plus\Roof_Survey_Table_for_Needham_DB.xlsx")
 sheet = book.sheet_by_name("Load")
 #
 # Open MySQL connection 
@@ -16,32 +19,32 @@ database = mysql.connector.connect (host="greenneedham.org", user = "michael", p
 cursor = database.cursor()
 #
 # Set a query
-query = ("SELECT rs_street_number, rs_street_name from RESIDENCE r where rs_precinct = 'E' order by rs_street_name")
+#query = ("SELECT rs_street_number, rs_street_name from RESIDENCE r where rs_precinct = 'E' order by rs_street_name")
 #
 # Execute the query
-cursor.execute(query)
+#cursor.execute(query)
 #
 # Display selected fields from result
-for (rs_street_number, rs_street_name) in cursor:
-  print("{} {} ".format(
-    rs_street_number, rs_street_name))
+#for (rs_street_number, rs_street_name) in cursor:
+#  print("{} {} ".format(
+#    rs_street_number, rs_street_name))
 #
 # Set an update query
-#update_query = ("UPDATE needham.RESIDENCE SET rs_has_PV = 'Y' WHERE rs_street_number like %s AND rs_street_name LIKE %s")
+update_query = ("UPDATE needham.RESIDENCE SET rs_pv_potl = %s WHERE rs_street_number like %s AND rs_street_name LIKE %s")
 #
 # Loop through the rows of the spreadsheet
 #  Read select values from each row into query parms (st_num, st_name)
 #  Execute the update query (set rs_has_PV to "Y")
 #
-#for r in range(1, sheet.nrows):
-#        st_num = str(int(sheet.cell(r,0).value))
-#        st_name = sheet.cell(r,1).value
-#        
-#        values = (st_num, st_name)
-#        print (st_num, st_name)
-#
-#
-#        cursor.execute(query, values)
+for r in range(1, sheet.nrows):
+  st_num = str(int(sheet.cell(r,0).value))
+  st_name = sheet.cell(r,1).value
+  pv_potl = sheet.cell(r,2).value
+
+  values = (pv_potl, st_num, st_name)
+  print (f" {st_num} {st_name} has solar potential = {pv_potl} ")
+
+  cursor.execute(update_query, values)
 #
 # Commit changes
-# database.commit()
+database.commit()
